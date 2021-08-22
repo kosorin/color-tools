a4af7094cd3d06d415f2d461a9573d7d5569dee0
David Kosorin <david@kosorin.net>
2021-08-22 21:13:27 +0200

    Add project files

---
+++ src/ColorTools/ColorPicker.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ColorTools
+{
+    public class ColorPicker : Control
+    {
+        static ColorPicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorPicker), new FrameworkPropertyMetadata(typeof(ColorPicker)));
+        }
+    }
+}
